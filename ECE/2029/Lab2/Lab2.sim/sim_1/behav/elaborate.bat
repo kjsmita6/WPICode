3ae05e089f577f1b7067296205458f58a089f7c1
Kyle Smith <dragonbanshee@gmail.com>
2020-03-11 11:20:13 -0500

    Clean up 2029

--- ECE/2029/Lab2/Lab2.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xelab  -wto f8035b9f31d74c449746cffbd43dbe45 -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot tb_and_or_not_xor_behav xil_defaultlib.tb_and_or_not_xor xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
