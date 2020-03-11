c068491ba49cf5d26f28134baf10284228113bc0
Kyle Smith <dragonbanshee@gmail.com>
2020-03-11 11:17:10 -0500

    clean up 2049

--- ECE/2049/ECE2049-Lab1/Debug/ece2049_demo_ccs81_smj/fonts/fontfixed6x8.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-ece2049_demo_ccs81_smj/fonts/fontfixed6x8.obj: ../ece2049_demo_ccs81_smj/fonts/fontfixed6x8.c
-
-../ece2049_demo_ccs81_smj/fonts/fontfixed6x8.c: 
--- ECE/2049/ECE2049-Lab1/Debug/ece2049_demo_ccs81_smj/images/LPRocket_96x37.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-ece2049_demo_ccs81_smj/images/LPRocket_96x37.obj: ../ece2049_demo_ccs81_smj/images/LPRocket_96x37.c
-
-../ece2049_demo_ccs81_smj/images/LPRocket_96x37.c: 
